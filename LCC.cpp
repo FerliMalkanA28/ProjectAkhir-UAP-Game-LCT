2ef7bea4a7315f2119dad2aae83e9cfc5d03a1e1
FerliMalkanAmien <92583458+FerliMalkanAmien@users.noreply.github.com>
2021-11-26 20:41:32 +0700

    Add files via upload

---
+++ LCC.cpp
@@ -0,0 +1,60 @@
+// Project Cerdas Cermat
+
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	// Deklarasi seluruh variabel
+	int a, b, c, hasil;
+	char mulai;
+	
+	
+	// Halaman Awal
+	cout << "=================================== \n";	
+	cout << "Selamat Datang di Game LCT Komputer \n";
+	cout << "=================================== \n";
+	
+	cout << endl << endl << endl;
+	
+	cout << "Bukan hanya tentang keseruan, tapi juga \n";
+	cout << "tentang kebermanfaatan. \n";
+	
+	cout << endl << endl;
+	
+	cout << "- Ferli Malkan Amien (2117051050) - \n";
+	cout << "- Ahmad Irvandi (21170510--) -" << endl << endl;
+	
+	cout << "Mulai (y/n): ";
+	cin  >> mulai;
+	
+	if (mulai == 'y' || mulai == 'Y'){
+		system("CLS");
+		
+		cout << "Peraturan Permainan: ";
+		cout << "1. Dilarang bekerja sama \n";
+		cout << "2. Dilarang joki \n";
+		cout << "3. Diperbolehkan menggunakan google" << endl << endl;
+		
+		cout << "- Press any key to continue -";
+		
+		getch();
+		
+		system("CLS");
+		
+		cout << "1. Diketahui ";
+	}
+	
+	else{
+		exit(0);
+	}
+	
+	
+	return 0;
+	
+	
+	
+}
