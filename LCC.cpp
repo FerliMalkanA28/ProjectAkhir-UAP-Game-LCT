1a46d8ec918223640fb63479db0f9b0ff1334d5d
ahmadirvandi7 <92698735+ahmadirvandi7@users.noreply.github.com>
2021-12-01 08:49:04 +0700

    Update LCC.cpp

--- LCC.cpp
+++ LCC.cpp
@@ -87,10 +87,65 @@ int main()
 		cout << endl << endl;
 		
 		// Soal Nomor 2
-		cout << "2. " << endl;
-		cout << "a. " << endl;
-		cout << "b. " << endl;
-		cout << "c. " << endl;
+		cout << "2. Waktu akses pada RAM adalah waktu yang dibutuhkan untuk melakukan.... " << endl;
+		cout << "a. Operasi baca " << endl;
+		cout << "b. Operasi tulis" << endl;
+		cout << "c. Operasi baca atau tulis" << endl;
+		cout << "Jawab : ";
+		cin  >> jawaban;
+		
+		poin_c(jawaban);
+		
+		cout << endl << endl;
+		
+		//Soal Nomor 3
+		cout << "3. Bagian dari CPU yang berfungsi menghubungkan seluruh bagian dari CPU adalah ...." << endl;
+		cout << "a. Cpu interconnection" << endl;
+		cout << "b. Arithmetic and logic unit" << endl;
+		cout << "c. Control unit" << endl;
+		cout << "Jawab : ";
+		cin  >> jawaban;
+		
+		poin_a(jawaban);
+		
+		cout << endl << endl;
+		
+		//Soal Nomor 4
+		cout << "4. Port yang digunakan untuk menambahkan perangkat-perangkat seperti VGA card, audio card, LAN card, dan firewire adalah...." << endl;
+		cout << "a. Port USB" << endl;
+		cout << "b. Port ekspansi" << endl;
+		cout << "c. Port serial" << endl;
+		cout << "Jawab : ";
+		cin  >> jawaban;
+		
+		poin_b(jawaban);
+		
+		cout << endl << endl;
+		
+		//Soal Nomor 5
+		cout << "5. Versi pertama Microsoft Windows, yang disebut dengan Windows1.0 dirilis pada tanggal..." << endl;
+		cout << "a. 18 januari 1984" << endl;
+		cout << "b. 19 maret 1983" << endl;
+		cout << "c. 20 november 1985" << endl;
+		cout << "Jawab : ";
+		cin  >> jawaban;
+		
+		poin_c(jawaban);
+		
+		cout << endl << endl;
+		
+		//Soal Nomor 6
+		cout << "6. Alamat-alamat dalam halaman web dikenal dengan nama..." << endl;
+		cout << "a. IP" << endl;
+		cout << "b. URL" << endl;
+		cout << "c. HTTP" << endl;
+		cout << "Jawab : ";
+		cin  >> jawaban;
+		
+		poin_b(jawaban);
+		
+		cout << endl << endl;
+		
 		
 		getch();
 	}
